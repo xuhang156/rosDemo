0379bb772ca4c0532eef5ca4c49cb1f10bd39764
Hang Xu <hanger0801@gmail.com>
2022-08-25 22:37:43 +0800

    feat: 添加bash脚本头

--- mk_default_workspace.sh
+++ mk_default_workspace.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 mkdir -p catkin_ws/src
 cd catkin_ws/src/
 catkin_init_workspace
@@ -6,12 +8,10 @@ catkin_make
 echo Make default workspace ok...
 
 sleep 1
-source devel/setup.bash
+source ./devel/setup.bash
 echo Souce the ros package path: $ROS_PACKAGE_PATH
 
 cd src
-read -r -p "Input the package name:(default dependecies: std_msgs rospy roscpp)" package
+read -r -p "Input the package name:(default dependecies: std_msgs rospy roscpp): " package
 catkin_create_pkg $package std_msgs rospy roscpp
 
-echo Show package: $package depends1...
-rospack depends1 $packeage
