b62a29e9fcc4915e8f08d034b91b55478e9dab1e
Shan Wang <hanger0801@gmail.com>
2022-08-27 10:27:45 +0800

    fix: 修复无法执行source命令；添加catkin_make命令

--- mk_default_workspace.sh
+++ mk_default_workspace.sh
@@ -7,11 +7,13 @@ cd ..
 catkin_make
 echo Make default workspace ok...
 
-sleep 1
-source ./devel/setup.bash
-echo Souce the ros package path: $ROS_PACKAGE_PATH
-
 cd src
 read -r -p "Input the package name:(default dependecies: std_msgs rospy roscpp): " package
 catkin_create_pkg $package std_msgs rospy roscpp
+cd ../
+
+sleep 1
+source ./devel/setup.bash
+echo Souce the ros package path: $ROS_PACKAGE_PATH
+catkin_make
 
